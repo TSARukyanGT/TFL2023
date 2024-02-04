569ea4585365b67571cf58b08e4a600cec1e5a64
Gevorg <tsarukyangt98@mail.ru>
2024-02-04 16:25:48 +0300

    Добавил перебор порядка состояний

--- Lab2Null/main.py
+++ Lab2Null/main.py
@@ -10,7 +10,7 @@ def getname(s):
     return res, ""
 text = open("input.txt", 'r')
 
-automat = []
+original_automat = []
 startcond, _ = getname(text.readline())
 finishcond = []
 secondline = text.readline()
@@ -27,126 +27,139 @@ for line in text:
     first, expr = getname(expr)
     path, expr = getname(expr)
     second, expr = getname(expr)
-    automat.append([first, path, second])
+    original_automat.append([first, path, second])
     if not second in conditions:
         conditions.append(second)
 conditions.extend(['S', 'F'])
-automat.append(['S', '$', startcond])
+original_automat.append(['S', '', startcond])
 for cond in finishcond:
-    automat.append([cond, '$', 'F'])
+    original_automat.append([cond, '', 'F'])
 
-condition_reg = {}
+original_cond_reg = {}
 condition_path_num = {}
 
-
 for cond in conditions:
-    condition_reg.update({cond: ""})
+    original_cond_reg.update({cond: ""})
 
-temp_automat = list(automat)
-for i in automat:
+temp_automat = list(original_automat)
+for i in original_automat:
     if i[0] == i[2]:
         if len(i[1]) == 1:
-            if condition_reg[i[0]] == "":
-                condition_reg[i[0]] += i[1] + "*"
-            elif condition_reg[i[0]][-2:] == ")*":
-                condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
+            if original_cond_reg[i[0]] == "":
+                original_cond_reg[i[0]] += i[1] + "*"
+            elif original_cond_reg[i[0]][-2:] == ")*":
+                original_cond_reg[i[0]] = original_cond_reg[i[0]][:-2] + "|" + i[1] + ")*"
             else:
-                condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
+                original_cond_reg[i[0]] = "(" + original_cond_reg[i[0]][:-1] + "|" + i[1] + ")*"
         else:
-            if condition_reg[i[0]] == "":
-                condition_reg[i[0]] += "("+i[1]+")*"
-            elif condition_reg[i[0]][-2:] == ")*":
-                condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
+            if original_cond_reg[i[0]] == "":
+                original_cond_reg[i[0]] += "("+i[1]+")*"
+            elif original_cond_reg[i[0]][-2:] == ")*":
+                original_cond_reg[i[0]] = original_cond_reg[i[0]][:-2] + "|" + i[1] + ")*"
             else:
-                condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
+                original_cond_reg[i[0]] = "(" + original_cond_reg[i[0]][:-1] + "|" + i[1] + ")*"
         temp_automat.remove(i)
-automat = list(temp_automat)
-
-maxpath = 0
-
-for cond in conditions:
-    condition_reg.update({cond: ""})
-
-temp_automat = list(automat)
-for i in automat:
-    if i[0] == i[2]:
-        condition_reg[i[0]] += "("+i[1]+")*"
-        temp_automat.remove(i)
-automat = list(temp_automat)
+original_automat = list(temp_automat)
 
 maxpath = 0
 for cond in conditions:
     pathnum = 0
-    for line in automat:
+    for line in original_automat:
         if line[0] == cond or line[2] == cond:
             pathnum += 1
     condition_path_num.update({cond: pathnum})
     if pathnum > maxpath:
         maxpath = pathnum
 
+orders = []
 ordered_conditions = []
 for i in range(0, maxpath+1):
     for cond in condition_path_num:
         if i == condition_path_num[cond] and cond != 'S' and cond != 'F':
             ordered_conditions.append(cond)
-            
-# ordered_conditions.remove(startcond)
-# ordered_conditions.append(startcond)
-# print(ordered_conditions)         #Состояния для исключения (в нужном порядке)
+orders.append(ordered_conditions)
+ord = list(ordered_conditions)
+ord.remove(startcond)
+ord.append(startcond)
+orders.append(ord)
+ord2 = list(ord)
+for f in finishcond:
+    ord2.remove(f)
+    ord2.append(f)
+orders.append(ord2)
+ord3 = list(ordered_conditions)
+for f in finishcond:
+    ord3.remove(f)
+    ord3.append(f)
+orders.append(ord3)
 
-# print(automat)                    #Автомат до изменения
+def accumulation(aut):
+    res_reg = ""
+    for rule in aut:
+        if res_reg != "":
+            res_reg += "|"
+        res_reg += rule[1]
+    return res_reg
 
-
-for deleting_cond in ordered_conditions:
-    additional_rules = []
-    for i in automat:
-        for o in automat:
-            if i[2] == deleting_cond and o[0] == deleting_cond:
-                additional_rules.append([i[0], i[1]+condition_reg[deleting_cond]+o[1], o[2]])
-    automat.extend(additional_rules)
-    temp_automat = list(automat)
-    for i in automat:
-        if (i[0] == deleting_cond) or (i[2] == deleting_cond):
-            temp_automat.remove(i)
-        elif i[0] == i[2]:
-            if len(i[1]) == 1:
-                if condition_reg[i[0]] == "":
-                    condition_reg[i[0]] += i[1] + "*"
-                elif condition_reg[i[0]][-2:] == ")*":
-                    condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
-                else:
-                    condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
-            else:
-                if condition_reg[i[0]] == "":
-                    condition_reg[i[0]] += "(" + i[1] + ")*"
-                elif condition_reg[i[0]][-2:] == ")*":
-                    condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
-                else:
-                    condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
-            temp_automat.remove(i)
-
-
-##### Начало "схлопывания" правил вида (1, a) -> 2
-####                                   (1, b) -> 2
-#                                       В (1, (a | b)) -> 2
-    automat = list(temp_automat)
-    temp_automat = list(automat)
-    for i in range(0 , len(automat)-1):
-        for j in range(i+1, len(automat)):
-            if (automat[i][0] == automat[j][0]) and (automat[i][2] == automat[j][2]) and (automat[i][1] != automat[j][1]):
-                temp_automat.remove(automat[i])
-                temp_automat.remove(automat[j])
-                temp_automat.append([automat[i][0], "("+automat[i][1]+"|"+automat[j][1]+")", automat[i][2]])
-
-##### Конец "схлопывания". Если закомментировать код посередине - будет правильный набор, но эти правила разделит на 2 части
-    automat = list(temp_automat)
-
-# print(automat)            # Итоговый автомат, где из состояний остались только S и F
 result_regex = ""
-for rule in automat:
-    if result_regex != "":
-        result_regex += " | "
-    result_regex += rule[1]
-result_regex = result_regex.replace('$', "")
+for ord_cons in orders:
+    condition_reg = dict(original_cond_reg)
+    automat = list(original_automat)
+    for deleting_cond in ord_cons:
+        additional_rules = []
+        for i in automat:
+            for o in automat:
+                if i[2] == deleting_cond and o[0] == deleting_cond:
+                    additional_rules.append([i[0], i[1]+condition_reg[deleting_cond]+o[1], o[2]])
+        automat.extend(additional_rules)
+        temp_automat = list(automat)
+        for i in automat:
+            if (i[0] == deleting_cond) or (i[2] == deleting_cond):
+                temp_automat.remove(i)
+
+            elif i[0] == i[2]:
+                if len(i[1]) == 1:
+                    if condition_reg[i[0]] == "":
+                        condition_reg[i[0]] += i[1] + "*"
+                    elif condition_reg[i[0]][-2:] == ")*":
+                        condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
+                    else:
+                        condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
+                else:
+                    if condition_reg[i[0]] == "":
+                        condition_reg[i[0]] += "(" + i[1] + ")*"
+                    elif condition_reg[i[0]][-2:] == ")*":
+                        condition_reg[i[0]] = condition_reg[i[0]][:-2] + "|" + i[1] + ")*"
+                    else:
+                        condition_reg[i[0]] = "(" + condition_reg[i[0]][:-1] + "|" + i[1] + ")*"
+                temp_automat.remove(i)
+
+        automat = list(temp_automat)
+        temp_automat = list(automat)
+        for i in range(0 , len(automat)-1):
+            for j in range(i+1, len(automat)):
+                if (automat[i][0] == automat[j][0]) and (automat[i][2] == automat[j][2]) and (automat[i][1] != automat[j][1]):
+                    temp_automat.remove(automat[i])
+                    temp_automat.remove(automat[j])
+                    word = ""
+                    if automat[i][1] == "":
+                        word = automat[j][1]
+                    elif automat[j][1] == "":
+                        word = automat[i][1]
+                    else:
+                        word = "("+automat[i][1]+"|"+automat[j][1]+")"
+                    temp_automat.append([automat[i][0], word, automat[i][2]])
+
+        automat = list(temp_automat)
+    reg = accumulation(automat)
+    if result_regex == "":
+        temp_reg = "Empty"
+    else:
+        temp_reg = result_regex
+    print("Сравнение {0} и {1}: Длина {2} против {3}".format(temp_reg, reg, len(result_regex), len(reg)))
+    if result_regex == "" or len(reg) < len(result_regex):
+        print("Замена {0} на {1}".format(temp_reg, reg))
+        result_regex = reg
+
 print("Result REGEX:")
 print(result_regex)
